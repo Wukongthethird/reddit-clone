37d52d38ac5060d516dfb93d5978943e11a3a2c0
Wukongthethird <simonzhang9518@gmail.com>
2022-06-14 16:49:01 -0700

    paginated posts with button disappearing when no more posts

--- lireddit-web/src/pages/index.tsx
+++ lireddit-web/src/pages/index.tsx
@@ -4,35 +4,59 @@ import { createUrqlClient } from "../utils/createUrqlClient";
 import { usePostsQuery } from "../generated/graphql";
 import { Layout } from "../components/Layout";
 import NextLink from "next/link";
-import { Box, Heading, Link, Stack, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Link,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+import { useState } from "react";
 
 const Index = () => {
-  const [{ data }] = usePostsQuery({
-    variables: {
-      limit: 10,
-    },
+  const [variables, setVariables] = useState({limit:10, cursor:null as null | string});
+  const [{ data, fetching }] = usePostsQuery({
+    variables,
   });
-
+  if (!fetching && !data) {
+    return <div> something failed</div>;
+  }
   return (
     <Layout>
-      <NextLink href="/create-post">
-        <Link>createpost</Link>
-      </NextLink>
-      {!data ? (
+      <Flex align="center">
+        <Heading>SNOO</Heading>
+        <NextLink href="/create-post">
+          <Link ml="auto">createpost</Link>
+        </NextLink>
+      </Flex>
+      {!data && fetching ? (
         <div>loading...</div>
       ) : (
         <Stack spacing={8}>
-          {data.posts.map((p) => (
+          {data!.posts.posts.map((p) => (
             <div key={p.id}>
-            <div >{p.title}</div>
-            <Box key={p.id} p={5} shadow="md" borderwidth = "1px">
-              <Heading fontSize="xl"> {p.title}</Heading>
-              <Text mt={4}> {p.textSnippet}</Text>
-            </Box>
+              <Box key={p.id} p={5} shadow="md" borderwidth="1px">
+                <Heading fontSize="xl"> {p.title}</Heading>
+                <Text mt={4}> {p.textSnippet}</Text>
+              </Box>
             </div>
           ))}
         </Stack>
       )}
+      {data && data.posts.hasMore ? (
+        <Flex>
+          <Button onClick={()=>{
+            setVariables({
+              limit: variables.limit,
+              cursor: data.posts.posts[data.posts.posts.length-1].createdAt
+            })
+          }} isLoading={fetching} m="auto" my={8}>
+            load more
+          </Button>
+        </Flex>
+      ) : null}
     </Layout>
   );
 };
--- lireddit-web/src/utils/createUrqlClient.ts
+++ lireddit-web/src/utils/createUrqlClient.ts
@@ -23,6 +23,106 @@ const errorExchange: Exchange =
       })
     );
   };
+
+  import { stringifyVariables } from '@urql/core';
+import { Resolver, Variables, NullArray } from '../types';
+
+export type MergeMode = 'before' | 'after';
+
+export interface PaginationParams {
+  offsetArgument?: string;
+  limitArgument?: string;
+  mergeMode?: MergeMode;
+}
+
+export const cursorPagination = (): Resolver => {
+ 
+  return (_parent, fieldArgs, cache, info) => {
+    const { parentKey: entityKey, fieldName } = info;
+    console.log( entityKey, fieldName)
+    const allFields = cache.inspectFields(entityKey);
+    console.log('ALLFIELDS', allFields)
+    const fieldInfos = allFields.filter(info => info.fieldName === fieldName);
+    const size = fieldInfos.length;
+    if (size === 0) {
+      return undefined;
+    }
+    
+    // allows for pagination check if there is data in the cache
+    const fieldkey = `${fieldName}(${stringifyVariables(fieldArgs)})`
+    const isItInTheCache = cache.resolve( entityKey ,fieldkey) 
+
+    info.partial = !isItInTheCache;
+
+    let hasMore = true;
+    
+    const results:string[] = [];
+    fieldInfos.forEach((fi:any) =>{
+      const key = cache.resolve(entityKey, fi.fieldKey) as string[];
+      const data = cache.resolve(key, 'posts')
+      const _hasMore = cache.resolve(key, 'hasMore')
+      if(!_hasMore){
+        hasMore = _hasMore as boolean
+      }
+      results.push(...data)
+    
+    })
+    console.log('thing', {hasMore, posts:results})
+    
+    return {__typename: "PaginatedPosts",hasMore, posts:results}
+    // const visited = new Set();
+    // let result: NullArray<string> = [];
+    // let prevOffset: number | null = null;
+
+    // for (let i = 0; i < size; i++) {
+    //   const { fieldKey, arguments: args } = fieldInfos[i];
+    //   if (args === null || !compareArgs(fieldArgs, args)) {
+    //     continue;
+    //   }
+
+    //   const links = cache.resolve(entityKey, fieldKey) as string[];
+    //   const currentOffset = args[offsetArgument];
+
+    //   if (
+    //     links === null ||
+    //     links.length === 0 ||
+    //     typeof currentOffset !== 'number'
+    //   ) {
+    //     continue;
+    //   }
+
+    //   const tempResult: NullArray<string> = [];
+
+    //   for (let j = 0; j < links.length; j++) {
+    //     const link = links[j];
+    //     if (visited.has(link)) continue;
+    //     tempResult.push(link);
+    //     visited.add(link);
+    //   }
+
+    //   if (
+    //     (!prevOffset || currentOffset > prevOffset) ===
+    //     (mergeMode === 'after')
+    //   ) {
+    //     result = [...result, ...tempResult];
+    //   } else {
+    //     result = [...tempResult, ...result];
+    //   }
+
+    //   prevOffset = currentOffset;
+    // }
+
+    // const hasCurrentPage = cache.resolve(entityKey, fieldName, fieldArgs);
+    // if (hasCurrentPage) {
+    //   return result;
+    // } else if (!(info as any).store.schema) {
+    //   return undefined;
+    // } else {
+    //   info.partial = true;
+    //   return result;
+    // }
+  };
+};
 // const errorExchange: Exchange =
 //   ({ forward }) =>
 //   (ops$) => {
@@ -46,6 +146,14 @@ export const createUrqlClient = (ssrExhange: any) => ({
   exchanges: [
     dedupExchange,
     cacheExchange({
+      keys:{
+        PaginatedPosts: ()=>null
+      },
+      resolvers:{
+        Query:{
+          posts:cursorPagination(),
+        }
+      },
       updates: {
         Mutation: {
           logout: (_result, args, cache, info) => {
